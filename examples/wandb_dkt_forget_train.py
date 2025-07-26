961b39481ff7350886400a156879a2f365c082d9
fresslinsa <2410185009@mails.szu.edu.cn>
2025-07-26 20:13:37 +0800

    细微调整

--- examples/wandb_dkt_forget_train.py
+++ examples/wandb_dkt_forget_train.py
@@ -11,13 +11,12 @@ if __name__ == "__main__":
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--fold", type=int, default=0)
     parser.add_argument("--dropout", type=float, default=0.2)
-    
+
     parser.add_argument("--emb_size", type=int, default=200)
     parser.add_argument("--learning_rate", type=float, default=1e-3)
 
     parser.add_argument("--use_wandb", type=int, default=1)
     parser.add_argument("--add_uuid", type=int, default=1)
-   
     args = parser.parse_args()
 
     params = vars(args)
